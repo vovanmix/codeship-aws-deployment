3aaa533831f1e314efc58a79c3bde7d775ff75fe
Lester Soriano <lestersoriano@Lesters-MacBook-Pro.local>
2017-06-29 14:32:35 +0800

    Remove asz

--- scripts/ecs-preconfigure.sh
+++ scripts/ecs-preconfigure.sh
@@ -19,7 +19,6 @@ ecs-cli up --capability-iam --verbose \
   --keypair $AWS_EC2_KEYPAIR \
   --size $AWS_ECS_SIZE \
   --instance-type $AWS_EC2_INSTANCE_TYPE \
-  --azs $AWS_EC2_AZS \
   --security-group $AWS_EC2_SECURITY_GROUP \
   --vpc $AWS_EC2_VPC \
   --subnets $AWS_EC2_SUBNETS
